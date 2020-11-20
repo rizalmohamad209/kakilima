c0292f60ea2103a49aeb631eacfc5f12b35fe517
rizal-mohamad <rizalmohamad209@gmail.com>
2020-11-21 05:11:11 +0700

    bismillahfixxxx

--- src/components/pedagang/Nav.js
+++ src/components/pedagang/Nav.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 import {faBars, faConciergeBell, faUser} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import Logo from '../../assets/images/logo.png'
